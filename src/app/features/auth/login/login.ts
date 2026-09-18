e1f4b1f5b2700d4918c3d0457fa358319e9788ce
thoratprashant <prashant.thorat@arkenea.com>
2026-09-18 11:20:00 +0530

    Added flash screen

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnDestroy, signal } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { LoaderService } from '../../../shared/loader/loader.service';
@@ -9,14 +9,20 @@ import { LoaderService } from '../../../shared/loader/loader.service';
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {
+export class Login implements OnDestroy {
   protected readonly hasCredentialsError = signal(false);
+  protected readonly showSplash = signal(true);
   protected submitted = false;
   protected showPassword = false;
   protected email = '';
   protected password = '';
+  private readonly splashTimer = window.setTimeout(() => this.showSplash.set(false), 2600);
 
   constructor(private readonly loader: LoaderService) {}
+
+  ngOnDestroy(): void {
+    window.clearTimeout(this.splashTimer);
+  }
 
   protected login(form: NgForm): void {
     this.submitted = true;
@@ -32,7 +38,7 @@ export class Login {
     window.setTimeout(() => {
       this.loader.hide();
       this.hasCredentialsError.set(true);
-    }, 500000);
+    }, 5000);
   }
 
   protected togglePassword(): void {
